8ebe38b365ee189b87ac9f6a36913f908865d469
Rhyhanop <118273732+Rhyhanop@users.noreply.github.com>
2026-02-12 02:07:58 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Navbar } from './components/Navbar';
+import { Hero } from './components/Hero';
+import { Services } from './components/Services';
+import { Pricing } from './components/Pricing';
+import { Order } from './components/Order';
+import { Reviews } from './components/Reviews';
+import { Trust } from './components/Trust';
+import { FAQ } from './components/FAQ';
+import { Contact } from './components/Contact';
+import { Footer } from './components/Footer';
+import { ExtraFeatures } from './components/ExtraFeatures';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <main>
+        <Hero />
+        <Services />
+        <Pricing />
+        <Order />
+        <Reviews />
+        <Trust />
+        <FAQ />
+        <Contact />
+      </main>
+      <Footer />
+      <ExtraFeatures />
+    </>
+  )
+}
+
+export default App
